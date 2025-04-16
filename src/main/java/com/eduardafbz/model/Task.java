af18ac16198c7a6a128a041e829694345d9355dd
Maria Eduarda De Faria Braz <E711126@edp.com.br>
2025-04-16 10:27:53 -0300

    feat: implementa preenchimento da classe Task

--- src/main/java/com/eduardafbz/model/Task.java
+++ src/main/java/com/eduardafbz/model/Task.java
@@ -1,5 +1,82 @@
 package com.eduardafbz.model;
 
-public class Task {
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Task implements Serializable {
+    private static final long serialVersionUID = 1L;
     
+    private Long id;
+    private String title;
+    private String description;
+    private boolean completed;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Task() {
+    }
+
+    public Task(Long id, String title, String description, boolean completed, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.completed = completed;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdateAt() {
+        return updatedAt;
+    }
+
+    public void mark_completed(){
+        this.completed = true
+    }
+
+    public void mark_uncompleted() {
+        this.completed = false
+    }
+    
+    @Override
+    public long hashCode() {
+        return Objects.hash(id)
+    }
+
+    @Override
+    public String toString() {
+        return "Task (id = {}, title = {}, description = {}, completed = {}, created at = {}, updated at = {}".format(id, title, description, completed, createdAt, updatedAt)
+    }    
 }
