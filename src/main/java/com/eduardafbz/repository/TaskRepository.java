6b33e0a0b993ec983731a04b94a9b6669591d826
Maria Eduarda De Faria Braz <E711126@edp.com.br>
2025-04-16 10:38:15 -0300

    feat: implementa preenchimento da interface TaskRepository

--- src/main/java/com/eduardafbz/repository/TaskRepository.java
+++ src/main/java/com/eduardafbz/repository/TaskRepository.java
@@ -1,5 +1,12 @@
 package com.eduardafbz.repository;
 
-public class TaskRepository {
+public interface TaskRepository {
     
+    void add(Task task)
+    List<Task> get_all();
+    Task get_by_id(int id);
+    void update(Task task);
+    void delete_all(List<Task>)
+    void delete_by_id(int id)
+
 }
