3b94239128cb252ce55890f14ee064eca8c3c779
Maria Eduarda De Faria Braz <E711126@edp.com.br>
2025-04-16 10:47:41 -0300

    feat: implementa preenchimento da classe TaskService

--- src/main/java/com/eduardafbz/service/TaskService.java
+++ src/main/java/com/eduardafbz/service/TaskService.java
@@ -1,5 +1,37 @@
 package com.eduardafbz.service;
 
+import com.eduardafbz.repository.TaskRepository;
+
 public class TaskService {
     
+    public final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public Task add(Task t) {
+        return taskRepository.add(t);
+    }
+
+    public List<Task> get_all() {
+        return taskRepository.get_all();
+    }
+
+    public Task get_by_id(int id) {
+        return taskRepository.get_by_id(id);
+    }
+
+    public Task update(Task t) {
+        return taskRepository.update(t);
+    }
+
+    public List<Task> delete_all() {
+        return taskRepository.delete_all();
+    }
+
+    public Task delete_by_id(int id) {
+        return taskRepository.delete_by_id(id);
+    }
+
 }
